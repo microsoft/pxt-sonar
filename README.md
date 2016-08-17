9b22c0bc8c5e280cf5e806ea48e9b9b7ac8c09a6
Peli de Halleux <jhalleux@microsoft.com>
2016-08-17 11:51:15 -0700

    initial sonar package

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# pxt-ping
-A PXT package to handle sonar sensors and pings
+# pxt-sonar
+A PXT package to use sonar sensors (ping) such as the HC-SR04
+
+## License
+MIT
+
+## Supported targets
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ main.ts
@@ -0,0 +1,38 @@
+enum PingUnit {
+    MicroSeconds,
+    Centimeters,
+    Inches
+}
+
+/**
+ * Sonar and ping utilities
+ */
+//% weight=10
+namespace sonar {
+    /**
+     * Send a ping and get the echo time (in microseconds) as a result
+     * @param trig tigger pin
+     * @param echo echo pin
+     * @param unit desired conversion unit
+     * @param maxCmDistance maximum distance in centimeters (default is 500)
+     */
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        let d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return d / 58;
+            case PingUnit.Inches: return d / 148;
+            default: return d ;
+        }
+    }
+}
